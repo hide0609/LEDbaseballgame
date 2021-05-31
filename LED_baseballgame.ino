e07f790e60449935f7d2606396e2e94c29ab9d54
hide06 <hide.kbknz.0609@gamail.com>
2021-05-31 19:07:43 +0900

    距離センサによるゲーム開始時のレベル調整機能を追加した。

--- LED_baseballgame.ino
+++ LED_baseballgame.ino
@@ -6,6 +6,7 @@
 #define BALL2 12
 #define BALL1 13
 #define DURATION 500000
+#define DIST 0
 
 unsigned long duration = DURATION;
 int level = 0;
@@ -40,11 +41,51 @@ boolean ball(int pin){
 }
 
 void loop(){
+  int t;
+  float k,v;
+  
   Serial.print("Level:");
   Serial.println(level);
   Serial.print("Strike:");
   Serial.println(strike);
   Serial.println("Hit the button tostart");
+
+//ゲームスタート時のレベル指定用条件式と、ボタンの設定
+  if(level==0 || strike==0 || last==0){
+    while(digitalRead(BUTTON)==HIGH){}
+    while(digitalRead(BUTTON)==LOW){
+
+//距離センサの値を読み取り、その値から実際の距離を算出する
+
+      t=analogRead(DIST);
+      v=(float)t*(float)0.0048;
+      k=(float)25.352*pow(v,-0.999);
+      
+//距離が10cm以下の時は10cm、80cm以上の時は80cmに固定する
+      if(k<10)
+        k=10;
+      else if(k>80)
+        k=80;
+   
+//距離が30cm以下の時、0.25s毎にレベルを1上げていく
+      if(k<30 && level!=20){
+        ++level;
+        duration*=0.8;
+        strike=0;
+        Serial.print(k,0);
+        Serial.println("[cm]");
+        Serial.print("Level:");
+        Serial.println(level);
+        Serial.print("Strike:");
+        Serial.println(strike);
+      }
+      
+      delay(250);
+      
+    }
+  
+  }
+  delay(50);
 
 //ボタンが押されてから離されるのを待つ
   while(digitalRead(BUTTON)==HIGH){}
@@ -84,6 +125,12 @@ void loop(){
     delay(700);
     tone(SPEAKER,50,500);
     while(digitalRead(BUTTON)==HIGH){}
+    duration=DURATION;
+    level=strike=last=0;
+  }
+//level20をクリアしたとき、Perfect!!と表示し、ゲームを最初からにリセット
+  if(level==21){
+    Serial.println("Perfect!!");
     duration=DURATION;
     level=strike=last=0;
   }
